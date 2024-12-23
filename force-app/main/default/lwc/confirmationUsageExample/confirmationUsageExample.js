b29b4c72274626d9873ce70c0199eec3968408c4
DENIS CHIONG CALDERON <denis.chiong@hexagon.com>
2024-12-23 09:27:48 +0100

    confirmation dialog

---
+++ force-app/main/default/lwc/confirmationUsageExample/confirmationUsageExample.js
@@ -0,0 +1,20 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ConfirmationUsageExample extends LightningElement {
+    @api
+    theMessage = '';
+
+    async handleClick(event) {
+        event.preventDefault();
+        const confirmation = this.template.querySelector('c-confirmation-dialog');
+        let confirmResult = await confirmation.confirm('The selectd record will be deleted. Do you wish to proceed?');
+        if (confirmResult === true) {
+            //delete record
+            this.dispatchEvent(new ShowToastEvent({ message: 'Record has been deleted', variant: 'success' }));
+            this.dispatchEvent(new CustomEvent('confirm'));
+
+            console.log('Record deleted');
+        }
+    }
+}
